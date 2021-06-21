af10c2c3278516ad489e98310b94e9e14ed3b83d
unknown <rohithasatya314@gmail.com>
2021-06-21 23:51:02 +0530

    Length of a Line

---
+++ LineComparisionUC1.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LineComparisionUC1 {
+	public static void main(String[] args) {
+
+		//variables
+		int x1=3, y1=1, x2=1, y2=4;
+
+		//computation
+		double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+			System.out.println(length);
+		}
+	}
